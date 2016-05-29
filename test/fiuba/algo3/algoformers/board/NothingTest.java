21a3bc71770808187ba05c932c43a098712522d2
jazmin ferreiro <jazmin.ferreiro@despegar.com>
2016-05-29 10:08:12 -0300

    agregando movimientos diagonales

--- test/fiuba/algo3/algoformers/board/NothingTest.java
+++ test/fiuba/algo3/algoformers/board/NothingTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 public class NothingTest {
 	@Test
-	public void compareNothingWithNothing(){
+	public void compareNothingWithNothingTest(){
 		IContent nada1 = new Nothing();
 		IContent nada2 = new Nothing();
 		Assert.assertEquals(nada1,nada2);
