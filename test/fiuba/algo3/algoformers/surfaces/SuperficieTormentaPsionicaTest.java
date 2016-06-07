38d0b2e86e7a79ae74aeb00b1ecd176897f0023d
Marcelo <marcelomastroianni@gmail.com>
2016-06-07 18:14:56 -0300

    Test Modo Alterno Terrestre no cruza superficie Tormenta Psionica

--- test/fiuba/algo3/algoformers/surfaces/SuperficieTormentaPsionicaTest.java
+++ test/fiuba/algo3/algoformers/surfaces/SuperficieTormentaPsionicaTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import fiuba.algo3.model.algoformers.AlgoFormerFactory;
 import fiuba.algo3.model.algoformers.Algoformer;
+import fiuba.algo3.model.algoformers.ModeAlternalTerrestrial;
 import fiuba.algo3.model.algoformers.ModeHumanoid;
 import fiuba.algo3.model.algoformers.board.Board;
 import fiuba.algo3.model.algoformers.board.Cell;
@@ -21,6 +22,21 @@ public class SuperficieTormentaPsionicaTest {
 		Assert.assertEquals("La superficie del casillero deberia ser Superficie Tormenta Psionica", superficieTormentaPsionica, casillero.getSurface());		
 	}
 
+	@Test
+	public void testModoAlternoTerrestreNoCruzaSuperficieTormentaPsionica(){
+		Board tablero = new Board(20,20);
+		tablero.addCell(new Cell(new Position(3,3), new SuperficieTormentaPsionica()));
+		Algoformer optimus = AlgoFormerFactory.getOptimusPrime(new Position(2,3));		
+		tablero.add(optimus);
+		optimus.transform();
+		Assert.assertTrue(optimus.getActiveMode() instanceof ModeAlternalTerrestrial);
+		optimus.move(new Position(4,3), tablero);
+		Assert.assertTrue("Algoformer no deberia estar en la posicion (4,3)",tablero.isEmpty(new Position(4,3)));
+		Assert.assertFalse("Algoformer no deberia estar en la posicion (4,3)",optimus.getPosition().equals(new Position(4,3)));
+		Assert.assertTrue("Algoformer deberia estar en la posicion (2,3)",optimus.getPosition().equals(new Position(2,3)));
+		Assert.assertEquals("Algoformer deberia estar en la posicion (2,3)",tablero.getContent(new Position(2,3)),optimus);												
+	}
+	
 	@Test
 	public void testModoHumanoideNoCruzaSuperficieTormentaPsionica(){
 		Board tablero = new Board(20,20);
