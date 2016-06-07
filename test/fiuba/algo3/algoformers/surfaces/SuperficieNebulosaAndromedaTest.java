cf2e5f2a57c814b06ea54524f8fc52f277c95961
Marcelo <marcelomastroianni@gmail.com>
2016-06-07 17:51:52 -0300

    Test Modo Humanoide no cruza superficie Nebulosa Andromeda

--- test/fiuba/algo3/algoformers/surfaces/SuperficieNebulosaAndromedaTest.java
+++ test/fiuba/algo3/algoformers/surfaces/SuperficieNebulosaAndromedaTest.java
@@ -38,7 +38,21 @@ public class SuperficieNebulosaAndromedaTest {
 		Assert.assertTrue("Algoformer no deberia estar en la posicion (4,3)",tablero.isEmpty(new Position(4,3)));
 		Assert.assertFalse("Algoformer no deberia estar en la posicion (4,3)",optimus.getPosition().equals(new Position(4,3)));
 		Assert.assertTrue("Algoformer deberia estar en la posicion (2,3)",optimus.getPosition().equals(new Position(2,3)));
-		Assert.assertEquals("Algoformer deberia estar en la posicion (2,3)",tablero.getContent(new Position(2,3)),optimus);
-												
+		Assert.assertEquals("Algoformer deberia estar en la posicion (2,3)",tablero.getContent(new Position(2,3)),optimus);												
 	}
+	
+	@Test
+	public void testModoHumanoideNoCruzaSuperficieNebulosaAndromeda(){
+		Board tablero = new Board(20,20);
+		tablero.addCell(new Cell(new Position(3,3), new SuperficieNebulosaAndromeda()));
+		Algoformer optimus = AlgoFormerFactory.getOptimusPrime(new Position(2,3));		
+		tablero.add(optimus);
+		Assert.assertTrue(optimus.getActiveMode() instanceof ModeHumanoid);
+		optimus.move(new Position(4,3), tablero);
+		Assert.assertTrue("Algoformer no deberia estar en la posicion (4,3)",tablero.isEmpty(new Position(4,3)));
+		Assert.assertFalse("Algoformer no deberia estar en la posicion (4,3)",optimus.getPosition().equals(new Position(4,3)));
+		Assert.assertTrue("Algoformer deberia estar en la posicion (2,3)",optimus.getPosition().equals(new Position(2,3)));
+		Assert.assertEquals("Algoformer deberia estar en la posicion (2,3)",tablero.getContent(new Position(2,3)),optimus);							
+	}
+
 }
